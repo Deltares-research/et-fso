b5e8a65287db7eef5c6618bf6391856563bc4aff
Ruben Imhoff <ruben.imhoff@deltares.nl>
2023-11-02 18:31:10 +0100

    add uk comparison plot script

--- scripts/verification/plots/Plot_KGE_per_Basin.py
+++ scripts/verification/plots/Plot_KGE_per_Basin.py
@@ -35,6 +35,9 @@ methods = ["ksathorfrac100","ksathorfrac_AXA","ksathorfrac_RF","ksathorfrac_BRT"
 # Define which KGE is used (options are kge_orig and kge_modified)
 used_kge_metric = "kge_orig"
 
+# Give the shapefile of the UK
+uk_shape = "c:/Users/imhof_rn/OneDrive - Stichting Deltares/Documents/SITO/Enabling_Technologies/FSO/ET-FSO-2023/UK_shape"
+
 # Define the output filename where the resulting map will be stored
 output_filename = "p:/11209205-034-et-fso/Verification/Figs/KGE_comparison_map_kge_orig.csv"
 # output_filename = "p:/11209205-034-et-fso/Verification/Figs/KGE_comparison_map_kge_modified.csv"
@@ -42,7 +45,6 @@ output_filename = "p:/11209205-034-et-fso/Verification/Figs/KGE_comparison_map_k
 # Finally, also provide the text files of the color maps used in Crameri et al. (2020),
 # Nature Communications
 imola_colormap = "c:/Users/imhof_rn/OneDrive - Stichting Deltares/Documents/PhD/ScientificColourMaps6/imola/DiscretePalettes/imola50.txt"
-
 
 # -------------------------------------------------------------------------------- #
 # Open all results
@@ -77,9 +79,26 @@ results_for_plotting["ksathorfrac_FSO"] = results_for_plotting[method].copy()
 methods.append("ksathorfrac_FSO")
 
 
-# --------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------- #
+# Also calculate the difference between the methods and the KsatHorFrac 100 run
+# Add it to the results and methods list
+# -------------------------------------------------------------------------------- #
+methods_new = []
+for method in methods:
+    if method != "ksathorfrac100":
+        results_for_plotting[f"difference_{method}"] = results_for_plotting[method] - results_for_plotting["ksathorfrac100"]
+        methods_new.append(f"difference_{method}")
+
+for method in methods_new:
+    methods.append(method)
+
+
+# -------------------------------------------------------------------------------- #
 # Open the colormap(s)
-# --------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------- #
+###
+# Open the imola map
+###
 # Open the text file in read mode
 file = open(imola_colormap, "r")
 
@@ -101,8 +120,8 @@ file.close()
 imola_colormap_list = np.array(imola_colormap_list)
 
 # We only use 7 out of 50 colors. 
-# Use slicing to get every 7th row starting from 0
-imola_colormap_final = imola_colormap_list[::7]
+# Use slicing to get every 8th row starting from 0
+imola_colormap_final = imola_colormap_list[::8]
 
 
 # --------------------------------------------------------------------------- #
@@ -110,55 +129,50 @@ imola_colormap_final = imola_colormap_list[::7]
 # --------------------------------------------------------------------------- #
 # Set up the figure and subplots grid
 fig = plt.figure(figsize=(20, 10))
-gs = GridSpec(2, 3, figure=fig) 
+gs = GridSpec(2, 5, figure=fig) 
 
 # The color and normalization settings of the map
-# colors = ['#a50026', '#d73027', '#f46d43', '#e0f3f8', '#abd9e9', "#2171b5", "#08306b"]
-cmap = ListedColormap(imola_colormap_final)
+colors_diff = ['#f4a582','#fddbc7','#f7f7f7','#92c5de','#4393c3','#2166ac','#053061']
+colors_kge = imola_colormap_final
+cmap_kge = ListedColormap(colors_kge)
+cmap_diff = ListedColormap(colors_diff)
 
 # define the bins and normalize
-bounds = [-10, -0.4, 0, 0.2, 0.4, 0.6, 0.8, 1]
-# bounds = [-10, -0.4, -0.25, 0, 0.25, 0.5, 0.75, 1]
-norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+bounds_kge = [-10, -0.4, 0, 0.2, 0.4, 0.6, 0.8, 1]
+bounds_diff = [-10, -0.2, -0.1, 0.1, 0.2, 0.3, 0.4, 1]
+norm_kge = mpl.colors.BoundaryNorm(bounds_kge, cmap_kge.N)
+norm_diff = mpl.colors.BoundaryNorm(bounds_diff, cmap_diff.N)
 
 # Define the labels
-labels = ['< -0.4', '-0.4 - 0.0', '0.0 - 0.2', '0.2 - 0.4', '0.4 - 0.6', '0.6 - 0.8', '> 0.8']
-# labels = ['< -0.4', '-0.4 - -0.25', '-0.25 - 0.0', '0.0 - 0.25', '0.25 - 0.5', '0.5 - 0.75', '> 0.75'] # Difference label
+labels_kge = [
+    '< -0.4', '-0.4 - 0.0', '0.0 - 0.2', '0.2 - 0.4', '0.4 - 0.6', '0.6 - 0.8', '> 0.8'
+    ]
+labels_diff = ['< -0.2', '-0.2 - -0.1', '-0.1 - 0.1', '0.1 - 0.2', '0.2 - 0.3', '0.3 - 0.4', '> 0.4']
 
 # Loop through methods and create subplots
 for i, method in enumerate(methods):
-    ax = fig.add_subplot(gs[i // 3, i % 3])  # Adjust subplot indexing based on grid size
+    # Add one to skip the first column in the second row of the plot (because we don't plot
+    # the difference with ksathorfrac 100).
+    if i >= 5: 
+        i = i+1
+    ax = fig.add_subplot(gs[i // 5, i % 5])  # Adjust subplot indexing based on grid size
 
     # Define a Basemap for the UK for each subplot
-    m = Basemap(projection='merc', llcrnrlat=49, urcrnrlat=59, llcrnrlon=-11, urcrnrlon=2,
-                lat_ts=51, resolution='i', ax=ax) # Pass the axes object to Basemap
-    # Resolution = h for higher resolution
+    m = Basemap(
+        projection='merc', 
+        llcrnrlat=49.9, 
+        urcrnrlat=59, 
+        llcrnrlon=-8, 
+        urcrnrlon=1.8,
+        lat_ts=51, 
+        resolution='i', 
+        ax=ax
+        )
 
     # Draw map details
-    m.drawcoastlines(linewidth=0.3,  zorder=3)
-    m.drawcountries(linewidth=0.4,  zorder=3)
-    m.fillcontinents(color=(0.92, 0.92, 0.92), lake_color='white')
-    m.drawmapboundary(fill_color='lightblue')
-
-    # # draw parallels and meridians
-    # parallels = np.arange(49., 59., 1.0)
-    # meridians = np.arange(-11, 2., 1.0)
-    # m.drawparallels(
-    #     parallels, 
-    #     labels=[True, False, False, False], 
-    #     fontsize=17, 
-    #     linewidth=0.75, 
-    #     color='gray', 
-    #     dashes=[1, 3],
-    #     )
-    # m.drawmeridians(
-    #     meridians, 
-    #     labels=[False, False, False, True], 
-    #     fontsize=17, 
-    #     linewidth=0.75, 
-    #     color='gray', 
-    #     dashes=[1, 3]
-    #     )
+    m.readshapefile(
+        uk_shape, "uk", color="grey", linewidth=0.5,
+        )
 
     # Convert gauge locations to map projection coordinates
     x, y = m(lons, lats)
@@ -166,38 +180,68 @@ for i, method in enumerate(methods):
     ###
     # create the new map
     ###
-    # Plot points colored by KGE value for each method
-    sc = ax.scatter(
-        x, y, c=results_for_plotting[method], cmap=cmap, norm=norm, s=10, zorder=2
-        )
-
-    # Set the axis labels
-    # ax.set_xlabel('Longitude', fontsize=22, labelpad=30)
-    # ax.set_ylabel('Latitude', fontsize=22, labelpad=40);
+    if i < 5:
+        # Plot points colored by KGE value for each method
+        sc = ax.scatter(
+            x, y, c=results_for_plotting[method], cmap=cmap_kge, norm=norm_kge, 
+            s=18, zorder=2, edgecolors="grey", linewidth=0.5,
+            )
+    else:
+        # Plot the difference in KGE for each method
+        sc = ax.scatter(
+            x, y, c=results_for_plotting[method], cmap=cmap_diff, norm=norm_diff, 
+            s=18, zorder=2, edgecolors="grey", linewidth=0.5,
+            )
 
     # Add a title to each subplot
-    ax.set_title(f'Results for {method}')
+    if i < 5:
+        ax.set_title(f'Results for {method}', fontsize=15)
+    else:
+        ax.set_title(f'Difference', fontsize=15)
 
-    # Create the custom legend using Line2D
-    legend_elements = []
-    for i in range(len(imola_colormap_final)):
-        legend_elements.append(Line2D([0], [0], color='w', marker='o', 
-                                    markerfacecolor=imola_colormap_final[i], markersize=10, 
-                                    label=labels[i])
-                            )
+    # Turn off the frames around the map
+    ax.set_frame_on(False)
 
-    legend1 = ax.legend(
-        handles=legend_elements, 
-        loc="center", 
-        bbox_to_anchor=(1.12, 0.74), 
-        ncol=1, 
-        frameon=False, 
-        title=r"KGE value", 
-        title_fontsize=22,
-        labelspacing=1.0,
-        fontsize=17,
-        )      
-    #TODO: make the legend work for the entire figure instead of for each sub figure
+# Create the custom legend using Line2D
+legend_elements_kge = []
+for i in range(len(colors_kge)):
+    legend_elements_kge.append(Line2D([0], [0], color='white', marker='o', 
+                                markerfacecolor=colors_kge[i], 
+                                markersize=10, label=labels_kge[i])
+                        )
+
+legend_elements_diff = []
+for i in range(len(colors_diff)):
+    legend_elements_diff.append(Line2D([0], [0], color='white', marker='o', 
+                                markerfacecolor=colors_diff[i], 
+                                markersize=10, label=labels_diff[i])
+                        )
+
+
+fig.legend(
+    handles=legend_elements_kge, 
+    loc="upper left", 
+    bbox_to_anchor=(0.02, 0.9), 
+    ncol=1, 
+    frameon=False, 
+    title=r"KGE", 
+    title_fontsize=15, 
+    labelspacing=1.0, 
+    fontsize=12,
+    )      
+
+fig.legend(
+    handles=legend_elements_diff, 
+    loc="lower left", 
+    bbox_to_anchor=(0.02, 0.1), 
+    ncol=1, 
+    frameon=False, 
+    title=r"KGE difference", 
+    title_fontsize=15, 
+    labelspacing=1.0, 
+    fontsize=12,
+    )     
+
 # plt.savefig(outfile)
 # plt.close()                
 plt.show()
